4c8f8a2b712e619692814002ecba3e07c2110aa7
Antonio Dolce <dolce.antonio@gmail.com>
2021-02-21 17:14:32 +0100

    added app properties in configmap

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello-resteasy")
 public class GreetingResource {
+
+    @ConfigProperty(name = "myapp.message") 
+    String message;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello Nino";
+        return "Hello Nino " + message;
     }
 }
